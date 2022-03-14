ce84bdb05fad19e2865cd836a2e3498120e9e779
choi sung jae <seotjae@gmail.com>
2022-03-14 10:03:41 +0900

    수정

--- src/main/java/gudi/pro/eeem/service/MeetService.java
+++ src/main/java/gudi/pro/eeem/service/MeetService.java
@@ -9,6 +9,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class MeetService {
 
 	public String meetRegist(MultipartFile thum_file, HashMap<String, String> params, MultipartFile[] photos) {
+		//1.파라미터 dto로 옮기기
+		//1-1썸네일 사진 이름 변환
+		//1-2썸네일 사진 저장
+		
+		//2.모임등록하고 등록한 meet_num 가져오기
+		
+		//3.내용사진 저장
+		
+		//4.
 		
 		return null;
 	}
